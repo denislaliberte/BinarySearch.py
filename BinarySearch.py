83d6c4e8e2cd42af70f2ce0166c0472b3a8e56af
Denis Laliberté <denis@tp1.ca>
2015-01-05 10:33:29 -0500

    add item in the first position with larger list

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,14 +1,13 @@
 def BinarySearch(a,b):
-  maxIndex = len(b)
-  middleIndex = maxIndex/2
   if not b:
     return -1
-  if(a == b[middleIndex]):
-    return middleIndex
-  middleIndex = middleIndex/2
-  if(a == b[middleIndex]):
-    return middleIndex
-  middleIndex =((maxIndex -middleIndex) /2)+middleIndex 
-  if(a == b[middleIndex]):
-    return middleIndex
+  maxIndex = len(b)
+  while(maxIndex > 0 ):
+    middleIndex = maxIndex/2
+    if(a == b[middleIndex]):
+      return middleIndex
+    middleIndex =((maxIndex -middleIndex) /2)+middleIndex 
+    if(a == b[middleIndex]):
+      return middleIndex
+    maxIndex = middleIndex
   return -1
--- BinarySearch_test.py
+++ BinarySearch_test.py
@@ -10,12 +10,12 @@ class BinarySearch_test(unittest.TestCase):
   def test_item_in_first_position(self):
     self.assertEqual(BinarySearch(1,[1]),0)
     self.assertEqual(BinarySearch(1,[1,3,5]),0)
+    self.assertEqual(BinarySearch(1,[1,3,5,7]),0)
+    self.assertEqual(BinarySearch(1,[1,3,5,7,9]),0)
 
   def test_item_is_in_the_middle(self):
     self.assertEqual(BinarySearch(3,[1,3,5]),1)
+    self.assertEqual(BinarySearch(5,[1,3,5,7,9]),2)
 
   def test_item_is_in_the_second_middle(self):
     self.assertEqual(BinarySearch(7,[1,3,5,7,9]),3)
-
-
-
