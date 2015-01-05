aa076046dbe354e31b021bd5c46082f9bc68a291
Denis Laliberté <denis@tp1.ca>
2015-01-05 12:16:05 -0500

    remove duplication

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,14 +3,12 @@ def BinarySearch(a,b):
     return -1
   maxIndex = len(b)
   minIndex = 0
-  middleIndex = maxIndex/2
   while(maxIndex > minIndex):
+    middleIndex = (minIndex + maxIndex) /2
     if(a == b[middleIndex]):
       return middleIndex
     if(a > b[middleIndex]):
       minIndex = middleIndex
-      middleIndex =((maxIndex -middleIndex) /2)+middleIndex
     else:
       maxIndex = middleIndex
-      middleIndex = maxIndex/2
   return -1
