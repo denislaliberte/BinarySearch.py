a37ecd29acda4d5530635743f6aa3154bb7b487a
Denis Laliberté <denis@tp1.ca>
2015-01-05 10:36:35 -0500

    add test case in the first half

--- BinarySearch_test.py
+++ BinarySearch_test.py
@@ -13,9 +13,13 @@ class BinarySearch_test(unittest.TestCase):
     self.assertEqual(BinarySearch(1,[1,3,5,7]),0)
     self.assertEqual(BinarySearch(1,[1,3,5,7,9]),0)
 
+  def test_item_in_the_first_half(self):
+    self.assertEqual(BinarySearch(3,[1,3,5,7,9]),1)
+    self.assertEqual(BinarySearch(3,[1,3,5,7,9,11,13]),1)
+
   def test_item_is_in_the_middle(self):
     self.assertEqual(BinarySearch(3,[1,3,5]),1)
     self.assertEqual(BinarySearch(5,[1,3,5,7,9]),2)
 
-  def test_item_is_in_the_second_middle(self):
+  def test_item_is_in_the_second_half(self):
     self.assertEqual(BinarySearch(7,[1,3,5,7,9]),3)
