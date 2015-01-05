e6c866fde5f903ff746c0f08c7affdb9c7605f98
Denis Laliberté <denis@tp1.ca>
2015-01-05 11:23:01 -0500

    change min index

--- BinarySearch.py
+++ BinarySearch.py
@@ -8,6 +8,7 @@ def BinarySearch(a,b):
     if(a == b[middleIndex]):
       return middleIndex
     if(a > b[middleIndex]):
+      minIndex = middleIndex
       middleIndex =((maxIndex -middleIndex) /2)+middleIndex
     if(a == b[middleIndex]):
       return middleIndex
